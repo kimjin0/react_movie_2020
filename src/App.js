7145de0c3e3ec104081a05d6f1184175ab42d2e8
kimjin0 <kimjin0@naver.com>
2020-04-27 19:06:45 +0900

    state 작업 들어가기 전!!!

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 function Song({title, src, image}){
 	return (
 		<div>
 			<h2>{title}</h2>
 			<span>{src}</span>
+			<img src={image} alt={title} />
+
 			<hr/>
 		</div>			
-		)
+	)
+}
+
+Song.propTypes  = {
+	title	: PropTypes.string.isRequired,
+	src		: PropTypes.string.isRequired,
+	image	: PropTypes.string
+}
+
+function renderSong(item){
+	console.log(item);
+	return <Song key={item.id} title={item.title} src={item.src} image={item.image} />
 }
 
 const songList = [
-	{
+	{	id		: 1,
 		title	: "우리는",
 		src		: "https://vod.watv.org/watv_media_newsong/_definst_/mp4:202003/2020_03_06_175126_068.m4a/playlist.m3u8",
 		image	: "https://watvnewsong.org/VNewsongThumbNail/%EA%B2%BD%EC%9D%8C%EC%95%852019(%EA%B3%B5%EC%9A%A9).jpg"
 	},
-	{
+	{	id		: 2,
 		title	: "그리운 어머니 음성",
 		src		: "https://vod.watv.org/watv_media_newsong/_definst_/mp4:201907/2019_07_29_140357_537.m4a/playlist.m3u8",
 		image	: "https://watvnewsong.org/VNewsongThumbNail/%EA%B2%BD%EC%9D%8C%EC%95%852019(%EA%B3%B5%EC%9A%A9).jpg"
 	},
-	{
+	{	
+		id		: 3,
 		title	: "생며의 기도",
 		src		: "https://vod.watv.org/watv_media_newsong/_definst_/mp4:201907/2019_07_29_135843_082.m4a/playlist.m3u8",
 		image	: "https://watvnewsong.org/VNewsongThumbNail/%EA%B2%BD%EC%9D%8C%EC%95%852019(%EA%B3%B5%EC%9A%A9).jpg"
@@ -31,10 +46,18 @@ const songList = [
 function App() {
 	return (
 		<div>
-			{songList.map(item=> (
-				<Song title={item.title} src={item.src} image={item.image} />
-			))}
+			{
+				songList.map(item=> (
+					<Song
+						key={item.id}		
+						title={item.title} 
+						src={item.src} 
+						// image={item.image} 
+					/>
+				))
+				//songList.map(renderSong)
+			}
 		</div>
-  )
+	);
 }
 export default App;
