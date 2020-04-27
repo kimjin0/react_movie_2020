b41e894ec365ca1415aaacd11f8f8a95ddcf0075
kimjin0 <kimjin0@naver.com>
2020-04-27 12:15:12 +0900

    테스트를 위해 songList Data 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from 'react';
 
-function App() {
-  return <div>Hello World!!</div>;
+function Song({title, src, image}){
+	return (
+		<div>
+			<h2>{title}</h2>
+			<span>{src}</span>
+			<hr/>
+		</div>			
+		)
 }
 
+const songList = [
+	{
+		title	: "우리는",
+		src		: "https://vod.watv.org/watv_media_newsong/_definst_/mp4:202003/2020_03_06_175126_068.m4a/playlist.m3u8",
+		image	: "https://watvnewsong.org/VNewsongThumbNail/%EA%B2%BD%EC%9D%8C%EC%95%852019(%EA%B3%B5%EC%9A%A9).jpg"
+	},
+	{
+		title	: "그리운 어머니 음성",
+		src		: "https://vod.watv.org/watv_media_newsong/_definst_/mp4:201907/2019_07_29_140357_537.m4a/playlist.m3u8",
+		image	: "https://watvnewsong.org/VNewsongThumbNail/%EA%B2%BD%EC%9D%8C%EC%95%852019(%EA%B3%B5%EC%9A%A9).jpg"
+	},
+	{
+		title	: "생며의 기도",
+		src		: "https://vod.watv.org/watv_media_newsong/_definst_/mp4:201907/2019_07_29_135843_082.m4a/playlist.m3u8",
+		image	: "https://watvnewsong.org/VNewsongThumbNail/%EA%B2%BD%EC%9D%8C%EC%95%852019(%EA%B3%B5%EC%9A%A9).jpg"
+	}		
+]
+
+function App() {
+	return (
+		<div>
+			{songList.map(item=> (
+				<Song title={item.title} src={item.src} image={item.image} />
+			))}
+		</div>
+  )
+}
 export default App;
