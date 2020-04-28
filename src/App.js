c4b368d91a090e9b6378162fd9887aff77a9cfa5
kimjin0 <kimjin0@naver.com>
2020-04-29 07:51:02 +0900

    4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from "prop-types";
-
+import axios from "axios";
 
 //** function component가 있는데 class component를 사용해야 하나?? >>> state 라고 불리는 녀석 때문이다.
 class App extends React.Component{
 	state = {
 		isLoading : true
 	};
+
+	getMovies = async () => {
+		const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+		console.log(movies);
+	};
+
 	componentDidMount(){
 		setTimeout(()=>{
 			this.setState({isLoading : false});
 		}, 6000);
+
+		this.getMovies();
 	}
+
 	render(){
 		const { isLoading } = this.state;
 		return <div>{isLoading ? "Loading...." : "We are ready"}</div>;
