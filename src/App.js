608cb6493fb66483c857538cec56d79feab54120
kimjin0 <kimjin0@naver.com>
2020-04-29 14:42:32 +0900

    Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,48 +1,58 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
-import Movie from "./Movies";
+import Movie from "./Movie";
+import "./App.css";
 
 //** function component가 있는데 class component를 사용해야 하나?? >>> state 라고 불리는 녀석 때문이다.
-class App extends React.Component{
-	state = {
-		isLoading : true,
-		movies : []
-	};
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
 
-	getMovies = async () => {
-		const {
-			data : {
-				data : {movies}
-			}
-		} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-		this.setState({movies, isLoading:false});
-	};
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies },
+            },
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+        this.setState({ movies, isLoading: false });
+    };
 
-	componentDidMount(){
-		// setTimeout(()=>{
-		// 	this.setState({isLoading : false});
-		// }, 6000);
+    componentDidMount() {
+        // setTimeout(()=>{
+        // 	this.setState({isLoading : false});
+        // }, 6000);
 
-		this.getMovies();
-	}
+        this.getMovies();
+    }
 
-	render(){
-		const { isLoading, movies } = this.state;
-		return <div>{isLoading ? "Loading...." : movies.map(movie => {
-			console.log(movie)
-			return <Movie 
-						key={movie.id}
-						id={movie.id} 
-						year={movie.year} 
-						title={movie.title} 
-						summary={movie.sumary} 
-						poster={movie.medium_cover_image}
-					/>
-		})}</div>;
-	}
+    render() {
+        const { isLoading, movies } = this.state;
+        return (
+            <section>
+                {isLoading ? (
+                    <div class="loader">
+                        <span class="loaer_text">Loading..</span>
+                    </div>
+                ) : (
+                    <div class="movies">
+                        {movies.map((movie) => (
+                            <Movie
+                                key={movie.id}
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                            />
+                        ))}
+                    </div>
+                )}
+            </section>
+        );
+    }
 }
-
-
 
 //#### 아래코드는 state 작업 전 코드 입니다.
 //### 주석을 잡아 class componet로 작성합니다.
@@ -53,7 +63,7 @@ class App extends React.Component{
 // 			<span>{src}</span>
 // 			<img src={image} alt={title} />
 // 			<hr/>
-// 		</div>			
+// 		</div>
 // 	)
 // }
 
@@ -79,12 +89,12 @@ class App extends React.Component{
 // 		src		: "https://vod.watv.org/watv_media_newsong/_definst_/mp4:201907/2019_07_29_140357_537.m4a/playlist.m3u8",
 // 		image	: "https://watvnewsong.org/VNewsongThumbNail/%EA%B2%BD%EC%9D%8C%EC%95%852019(%EA%B3%B5%EC%9A%A9).jpg"
 // 	},
-// 	{	
+// 	{
 // 		id		: 3,
 // 		title	: "생며의 기도",
 // 		src		: "https://vod.watv.org/watv_media_newsong/_definst_/mp4:201907/2019_07_29_135843_082.m4a/playlist.m3u8",
 // 		image	: "https://watvnewsong.org/VNewsongThumbNail/%EA%B2%BD%EC%9D%8C%EC%95%852019(%EA%B3%B5%EC%9A%A9).jpg"
-// 	}		
+// 	}
 // ]
 
 // function App() {
@@ -93,10 +103,10 @@ class App extends React.Component{
 // 			{
 // 				songList.map(item=> (
 // 					<Song
-// 						key={item.id}		
-// 						title={item.title} 
-// 						src={item.src} 
-// 						// image={item.image} 
+// 						key={item.id}
+// 						title={item.title}
+// 						src={item.src}
+// 						// image={item.image}
 // 					/>
 // 				))
 // 				//songList.map(renderSong)
